6bdf57b111898e85582a378c3aac807f94a554fb
Maneka <58079663+maneka582@users.noreply.github.com>
2021-08-25 16:54:16 +0530

    Listning Post

---
+++ example2/src/Components/Listing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Post from './Post';
+import './Post.css';
+ 
+class Listing extends Component {
+    showPosts = () => {
+        const posts = this.props.posts;
+        if (posts.length === 0) return null;        
+        return (
+            <div classname="post_list_item"><React.Fragment>
+                {Object.keys(posts).map(post =>(
+                    <Post
+                        key={post}
+                        info={this.props.posts[post]}
+                        deletePost={this.props.deletePost}
+                    />
+                    
